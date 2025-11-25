1df9461eff8a7b3b195004f20f32f085e9ec5354
develop <a@b.com>
2025-11-25 23:57:28 +0800

    update

--- extras/server-squashfs-modifications.sh
+++ extras/server-squashfs-modifications.sh
@@ -16,10 +16,9 @@ apt-get update && \
   apt-get install -y dpkg-dev bash curl gpg lsb-core software-properties-common proxychains4 && \
 #  sed -i -E 's/socks4.*9050/socks5         192.168.75.16 7896/g' /etc/proxychains4.conf && \
   # nvidia-container-toolkit
-  curl -fsSL https://nvidia.github.io/libnvidia-container/gpgkey | \
-  gpg --dearmor -o /usr/share/keyrings/nvidia-container-toolkit-keyring.gpg && \
-  curl -s -L https://nvidia.github.io/libnvidia-container/stable/deb/nvidia-container-toolkit.list | \
-  sed 's#deb https://#deb [signed-by=/usr/share/keyrings/nvidia-container-toolkit-keyring.gpg] https://#g' | \
+  curl -fsSL https://mirrors.ustc.edu.cn/libnvidia-container/gpgkey | gpg --dearmor -o /usr/share/keyrings/nvidia-container-toolkit-keyring.gpg && \
+  curl -s -L https://mirrors.ustc.edu.cn/libnvidia-container/stable/deb/nvidia-container-toolkit.list | \
+  sed 's#deb https://nvidia.github.io#deb [signed-by=/usr/share/keyrings/nvidia-container-toolkit-keyring.gpg] https://mirrors.ustc.edu.cn#g' | \
   tee /etc/apt/sources.list.d/nvidia-container-toolkit.list && \
   apt-get update &&  apt-get install -y nvidia-container-toolkit && \
 
